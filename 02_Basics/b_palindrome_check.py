98295bbb532b14ae86adbf7e3f3f9819c7393c65
manasaanna <mmanasaanna@gmail.com>
2024-11-27 17:51:24 +0000

    class07 - slicing

---
+++ 02_Basics/b_palindrome_check.py
@@ -0,0 +1,15 @@
+"""
+To check whether a string is palindrome or not 
+palindrome strings
+dad 
+mom 
+
+Algo 
+1) Take the string from runtime and store in variable 
+2) get the reverse string from the given string using the string reversal
+3)compare both with a condition 
+
+
+"""
+
+test_string = input()

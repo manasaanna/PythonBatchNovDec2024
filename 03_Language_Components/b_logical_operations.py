993dee5825636763e7308908a338018245038cfc
manasaanna <mmanasaanna@gmail.com>
2024-12-30 13:39:55 +0000

    class 10 - language components

---
+++ 03_Language_Components/b_logical_operations.py
@@ -0,0 +1,24 @@
+
+# and - If all are true, result is true; else False
+print("\n and operation ")
+print("True and True ", True and True) # True
+print("True and False ", True and False)
+print("False and True ", False and True)
+print("False and False ", False and False)
+print()
+
+
+# or - If all are False, result is False; else True
+print("\n or operation ")
+print("True or True ", True or True)
+print("True or False ", True or False)
+print("False or True ", False or True)
+print("False or False ", False or False) # False
+
+# not - Will negate the existing boolean result
+print("\n not operation ")
+print("not True ", not True)
+print("not False", not False)
+print("not not True = ", not not True)
+print("not not False = ", not not False)
+print()
